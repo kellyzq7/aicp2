d893cb26102d90c2c5f497034016a68198258b54
Bodie <86332414+Bod1eF@users.noreply.github.com>
2022-07-20 14:57:46 -0700

    Add files via upload

---
+++ drop.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Parkamon Dropper</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+      <?php
+      require_once "sql_config.php";
+      // PDO handaling documentaion https://www.php.net/manual/en/function.file-get-contents.php https://www.php.net/manual/en/pdo.exec.php
+      try {
+     $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
+     //drop tables
+     $query = file_get_contents('drop.sql');
+     $dbh->exec($query);
+     echo "<p>Tables Dropped</p>";
+ }
+     catch (PDOException $e) {
+     echo "<p>Error: {$e->getMessage()}</p>";
+        }
+
+
+      ?>
+  </body>
+</html>
