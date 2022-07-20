ca8d033cb27c95efb63e98a22bc10d542b6ee352
Bodie <86332414+Bod1eF@users.noreply.github.com>
2022-07-20 15:09:34 -0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int NOT NULL AUTO_INCREMENT,
   `isAdmin` boolean,
-  `username` text,
+  `email` text,
   `password` text,
   `character_name` varchar(16),
   `class` varchar(16),
