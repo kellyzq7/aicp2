cf4e8979663335dde0225300d171c27e5a132fa3
kellyzq7 <kellyzq7@gmail.com>
2022-07-20 20:31:20 -0700

    3 alternate endings
    no css

---
+++ boss.php
@@ -0,0 +1,31 @@
+<?php
+    session_start();
+    if(!isset($_SESSION["email"])){
+        echo "header";
+        header("Location: login.php");
+    }
+    try {
+      $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
+      $sth = $dbh->prepare("UPDATE user SET position=1 WHERE email=:login_email");
+      $sth->bindValue(':login_email', $_SESSION["email"]);
+      $sth->execute();
+    }
+    catch (PDOException $e) {
+      echo "<p>Error: {$e->getMessage()}</p>";
+    }
+ ?>
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <title>Cul Cavboj</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+  </head>
+  
+  <body>
+    <h1>Meet your match: The Boss</h1>
+    <p>something with combat (and possibly speed) points</p>
+  
+  
+  </body>
+</html>
+  
