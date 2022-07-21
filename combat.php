93f3595834362d6231e14698259e6729d465265c
Bodie <86332414+Bod1eF@users.noreply.github.com>
2022-07-20 19:47:23 -0700

    Update combat.php

--- combat.php
+++ combat.php
@@ -19,33 +19,49 @@
   </head>
   <body>
     <?php
-    require_once "credentials.php";
+  session_start();
+  require_once "sql_config.php";
+    if(!isset($_SESSION["email"])){
+     echo "header";
+     header("Location: login.php");
+   }
+
+    try {
+      $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
+      $sth = $dbh->prepare("UPDATE user SET `position`=2 WHERE email=:login_email");
+      $sth->bindValue(':login_email', $_SESSION["email"]);
+      $sth->execute();
+      }
+    catch (PDOException $e) {
+      echo "<p>Error: {$e->getMessage()}</p>";
+                }
     try {
         $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
         $email = $_SESSION["email"];
-        
+
         $sth = $dbh -> prepare("SELECT combat FROM user WHERE email = :email");
         $sth -> bindvalue(":email", $email);
         $sth -> execute();
         $combatPoints = $sth -> fetch();
-        
+
         if($combatPoints == 2){
           echo "<input id = 'big' type = 'button' value = 'FIGHT!' />";
           echo "<h2 class = 'hide'>Congrats! You won the combat! As a reward, you get 2 combat points!</h2>";
           echo "<a class = 'hide' href = 'decision_towns.php'>Onward!</a>";
-          
+
         } else {
           echo "<input id = 'small' type = 'button' value = 'FIGHT!' />";
           echo "<h2 class = 'hide'>Congrats! You won the combat! As a reward, you get 2 combat points!</h2>";
           echo "<a class = 'hide' href = 'decision_towns.php'>Onward!</a>";
         }
-        
-    
+
+
     } catch (PDOException $e) {
         echo "<p>Error: {$e->getMessage()}</p>";
     }
 
-    
+
     ?>
   </body>
 <html>
+
