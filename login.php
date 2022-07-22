362dbc85e5472ad7a9c796912a2edd339746d3e4
Bodie <86332414+Bod1eF@users.noreply.github.com>
2022-07-21 23:00:45 -0700

    Update login.php

--- login.php
+++ login.php
@@ -2,18 +2,32 @@
 session_start();
 require_once "sql_config.php";
 
+//account register
+$new_account = false;
+
 if (isset($_POST["email"]) && isset($_POST["password"]) == true) { //if coming from register, update user table to create account
+  if (filter_var(htmlspecialchars($_POST["email"]), FILTER_VALIDATE_EMAIL)) { //validates email is legit
   try {
   $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
-  $sth = $dbh->prepare("INSERT INTO user ( email, password, position)
-  VALUES (:reg_email, :reg_password, '0');");
+  $sth = $dbh->prepare("INSERT INTO user (`email`, `password`, `isAdmin`)
+  VALUES (:reg_email, :reg_password, false);"); //store account info into database
   $sth->bindValue(':reg_email', htmlspecialchars($_POST["email"]));
   $sth->bindValue(':reg_password', password_hash(htmlspecialchars($_POST["password"]), PASSWORD_DEFAULT));
   $sth->execute();
+  $sth_id = $dbh->prepare("SELECT `id` FROM user WHERE `email`=:reg_email"); //gets user row of newly created account
+  $sth_id->bindValue(':reg_email', htmlspecialchars($_POST["email"]));
+  $sth_id->execute();
+  $reg_id = $sth_id->fetch();
+  $sth_char = $dbh->prepare("INSERT INTO character (`user_id`, `position`)
+  VALUES (:reg_id, 0);"); //create character associated with account, and set defualt position to 0.
+  $sth_char->bindValue(':reg_id', $reg_id);
+  $sth_char->execute();
+  $new_account = true;
   }
   catch (PDOException $e) {
   echo "<p>Error: {$e->getMessage()}</p>";
             }
+}
 }
 ?>
 <!doctype html>
@@ -32,6 +46,13 @@ if (isset($_POST["email"]) && isset($_POST["password"]) == true) { //if coming f
   <?php
   ?>
 <h1> Account Login </h1>
+<?php
+
+if ($new_account == true) {
+  echo "<h2> Account Succesfully Created </h2>";
+}
+
+?>
 <form action="redirect.php" method="POST">
 <label for="user_login"> Email: </label>
 <input type="email" name="user_login" id="user_login">
