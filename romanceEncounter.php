4e241056d10383d0f1a8edc99cd829efeb3e04b3
kellyzq7 <kellyzq7@gmail.com>
2022-07-21 22:28:51 -0700

    Update romanceEncounter.php

--- romanceEncounter.php
+++ romanceEncounter.php
@@ -1,3 +1,23 @@
+<?php
+session_start();
+require_once "sql_config.php";
+
+//update their the data base with the user's character name and position
+if (isset($_SESSION["email"])) {//check if user is logged in
+  try {
+    $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
+    $sth = $dbh->prepare("UPDATE user SET `position`=4 WHERE email=:login_email");
+    $sth->bindValue(':login_email', $_SESSION["email"]);
+    $sth->execute();
+    }
+  catch (PDOException $e) {
+    echo "<p>Error: {$e->getMessage()}</p>";          
+  }
+}else {
+    header('Location: login.php'); //if user isn't signed in send to login
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -50,6 +70,8 @@ $charisma = 2;
         echo "<p id='great'>idk what to write here</p>";
         echo "<p id='response3'>James Jessie: Cue epic response 3</p>";
       }
+      
+      echo "<p>Click<a href = 'decision_towns1.php'>here</a> to move on</p>"
 }
 
 catch (PDOException $error){
