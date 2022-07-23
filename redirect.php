94b06d7edacee4628f2a434c21b95c39e4c0d029
Bodie <86332414+Bod1eF@users.noreply.github.com>
2022-07-22 20:14:43 -0700

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -44,82 +44,82 @@ echo "<p>Error: {$e->getMessage()}</p>";
   //Fetch Character location, and echo link redirecting to that page
   try {
   $_SESSION["email"] = htmlspecialchars($_POST["user_login"]);
-  $sth_location = $dbh->prepare("SELECT * FROM user WHERE email=:login_email");
+  $sth_location = $dbh->prepare("SELECT * FROM player_character WHERE email=:login_email");
   $sth_location->bindValue(':login_email', htmlspecialchars($_POST["user_login"]));
   $sth_location->execute();
-  $user = $sth_location->fetch();
+  $player = $sth_location->fetch();
 
-  if($user["position"] == 0) {
+  if($player["position"] == 0) {
     echo "<a class='redirect' href='class_select.php'> Create New Character </a>";
   }
-  else if($user["position"] == 1) {
+  else if($player["position"] == 1) {
     echo "<a class='redirect' href='intro.php'> Return to Game </a>";
   }
-  else if($user["position"] == 2) {
+  else if($player["position"] == 2) {
     echo "<a class='redirect' href='combat.php'> Return to Game </a>";
 }
-  else if($user["position"] == 3) {
+  else if($player["position"] == 3) {
     echo "<a class='redirect' href='decision_romancevcombat.php'> Return to Game catch </a>";
   }
-  else if($user["position"] == 4) {
+  else if($player["position"] == 4) {
     echo "<a class='redirect' href='romanceEncounter.php.'> Return to Game </a>";
   }
-  else if($user["position"] == 5) {
+  else if($player["position"] == 5) {
     echo "<a class='redirect' href='combat1.php'> Return to Game </a>";
   }
-  else if($user["position"] == 6) {
+  else if($player["position"] == 6) {
     echo "<a class='redirect' href='decision_towns1.php'> Return to Game </a>";
   }
-  else if($user["position"] == 7) {
+  else if($player["position"] == 7) {
     echo "<a class='redirect' href='decision_towns.php'> Return to Game </a>";
   }
-  else if($user["position"] == 8) {
+  else if($player["position"] == 8) {
     echo "<a class='redirect' href='yay.php'> Return to Game </a>";
   }
-  else if($user["position"] == 9) {
+  else if($player["position"] == 9) {
     echo "<a class='redirect' href='dodge1.php'> Return to Game </a>";
   }
-  else if($user["position"] == 10) {
+  else if($player["position"] == 10) {
     echo "<a class='redirect' href='dodge.php'> Return to Game </a>";
   }
-  else if($user["position"] == 11) {
+  else if($player["position"] == 11) {
     echo "<a class='redirect' href='dodge1.php'> Return to Game </a>";
   }
-  else if($user["position"] == 12) {
+  else if($player["position"] == 12) {
     echo "<a class='redirect' href='town1.php'> Return to Game </a>";
   }
-  else if($user["position"] == 13) {
+  else if($player["position"] == 13) {
     echo "<a class='redirect' href='town2.php'> Return to Game </a>";
 }
-  else if($user["position"] == 14) {
+  else if($player["position"] == 14) {
     echo "<a class='redirect' href='npc1.php'> Return to Game </a>";
   }
-  else if($user["position"] == 15) {
+  else if($player["position"] == 15) {
     echo "<a class='redirect' href=npc3.php'> Return to Game </a>";
 }
-  else if($user["position"] == 16) {
+  else if($player["position"] == 16) {
     echo "<a class='redirect' href='npc2.php'> Return to Game </a>";
   }
-  else if($user["position"] == 17) {
+  else if($player["position"] == 17) {
     echo "<a class='redirect' href='npc4.php'> Return to Game </a>";
   }
-  else if($user["position"] == 18) {
+  else if($player["position"] == 18) {
     echo "<a class='redirect' href='finaldecision1.php'> Return to Game </a>";
   }
-  else if($user["position"] == 19) {
+  else if($player["position"] == 19) {
     echo "<a class='redirect' href='finaldecision2.php'> Return to Game </a>";
   }
-  else if($user["position"] == 20) {
+  else if($player["position"] == 20) {
     echo "<a class='redirect' href='love.php'> Return to Game </a>";
   }
-  else if($user["position"] == 21) {
+  else if($player["position"] == 21) {
     echo "<a class='redirect' href='boss.php'> Return to Game </a>";
   }
-  else if($user["position"] == 22) {
+  else if($player["position"] == 22) {
     echo "<a class='redirect' href='freedom.php'> Return to Game </a>";
   }
   else { //if invalid location reset location and send to login
-      $sth_location = $dbh->prepare("UPDATE user SET position = 1 WHERE email=:login_email");
+      $sth_location = $dbh->prepare("UPDATE player_character SET position = 0 WHERE email=:login_email");
       $sth_location->bindValue(':login_email', htmlspecialchars($_POST["user_login"]));
       $sth_location->execute();
       $user = $sth_location->fetch();
