f605831669980b174e934153899a2d327569fa8a
Bodie <86332414+Bod1eF@users.noreply.github.com>
2022-07-22 19:35:24 -0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`id`)
 );
 
-CREATE TABLE IF NOT EXISTS `character` (
+CREATE TABLE IF NOT EXISTS `player_character` (
 `id` int NOT NULL AUTO_INCREMENT,
 `character_name` varchar(16),
 `class` varchar(16),
