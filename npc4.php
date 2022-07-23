57cef6be17c08b5df90575aeffdd451f3088ac82
kellyzq7 <kellyzq7@gmail.com>
2022-07-23 13:47:36 -0700

    update locations of npc.phps

--- npc4.php
+++ npc4.php
@@ -2,12 +2,13 @@
 session_start();
 require_once "sql_config.php";
 
-//update their the data base with the user's character name and position
-if (isset($_SESSION["email"])) {//check if user is logged in
+//check that user is signed in
+if (isset($_SESSION["email"]) && isset($_SESSION["player_id"])) {
   try {
     $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
-    $sth = $dbh->prepare("UPDATE user SET `position`=17 WHERE email=:login_email");
-    $sth->bindValue(':login_email', $_SESSION["email"]);
+    $sth = $dbh->prepare("UPDATE player_character SET `position`= 16 
+      WHERE id =:player_id");
+    $sth->bindValue(':player_id', $_SESSION["player_id"]);
     $sth->execute();
     }
   catch (PDOException $e) {
