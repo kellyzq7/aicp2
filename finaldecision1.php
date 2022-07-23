b93fb9c69ed34ea92eaaf93750df08aa34aa7bf4
kellyzq7 <kellyzq7@gmail.com>
2022-07-23 16:44:43 -0700

    Update finaldecision1.php

--- finaldecision1.php
+++ finaldecision1.php
@@ -23,7 +23,7 @@ if (isset($_SESSION["email"]) && isset($_SESSION["player_id"])) {
   <head>
     <title>Decision: FINAL PATH</title>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-    <link rel="stylesheet" href="finaldecision1.css">
+    <link rel="stylesheet" href="cssandjs/finaldecision1.css">
   </head>
   <body>
     <h1>Decision: Love Story or Boss Face Off</h1>
