114c19ffaebfde20d682c463d2af2fc0319a7693
kellyzq7 <kellyzq7@gmail.com>
2022-07-23 19:06:23 -0700

    charger, charmer, crasher updates

--- charmer.php
+++ charmer.php
@@ -1,19 +1,19 @@
 <?php
 session_start();
-require_once "sql_config.php";
+require_once "kelly_credentials.php";
 
 //update their the data base with the user's character stats and class
-if (isset($_SESSION["email"]) && isset($_SESSION["player_id"])) {//check if user is logged in and character is created + selected
+if (isset($_SESSION["email"])) {//check if user is logged in and character is created + selected
 try {
-  $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
-  $sth = $dbh->prepare("UPDATE player_character SET `class` = 'charmer', `combat`=0, `charisma`= 2, `celerity`= 0 WHERE id=:player_id"); //updates character with class + stats
-  $sth->bindValue('player_id', $_SESSION["player_id"]);
-  $sth->execute();
+    $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
+    $sth = $dbh->prepare("UPDATE player_character SET `class` = 'charmer', `combat`=0, `charisma`= 2, `celerity`= 0 WHERE id=:player_id"); //updates character with class + stats
+    $sth->bindValue('player_id', $_SESSION["player_id"]);
+    $sth->execute();
+    }
+  catch (PDOException $e) {
+    echo "<p>Error: {$e->getMessage()}</p>";
   }
-catch (PDOException $e) {
-  echo "<p>Error: {$e->getMessage()}</p>";
-            }
-          }
+}
 else {
   header('Location: login.php'); //if user isn't signed in send to login
 }
