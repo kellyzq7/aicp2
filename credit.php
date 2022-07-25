f68741c93f689d33088a06009159b38ebc6073ce
Bodie <86332414+Bod1eF@users.noreply.github.com>
2022-07-25 12:59:34 -0700

    Create credit.php

---
+++ credit.php
@@ -0,0 +1,76 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html lang="en">
+<head>
+<title>Credit</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script></script>
+<style>
+a {
+  text-align:center;
+  display: block;
+  margin:20px;
+  font-size:36px;
+}
+body {
+  font-family:sans-serif;
+  display:flex;
+  justify-content:center;
+  flex-direction:column;
+  align-items:center;
+}
+h2, h1 {
+  text-align:center;
+}
+h2 {
+  font-size:48px;
+  margin:10px;
+}
+h1 {
+  font-size:56px;
+  margin:15px;
+}
+p {
+  width:40%;
+  font-size:24px;
+  text-align:center;
+}
+#links {
+  display:flex;
+  justify-content:center;
+  flex-direction:column;
+  align-items:center;
+}
+</style>
+</head>
+<body>
+<h1> Credit Links </h1>
+<div id="links">
+<a href="login.php"> Back to Login </a>
+
+<h2 class="subheading"> Image Credit </h2>
+<a href="https://wallpaperaccess.com/cowboy-wild-west"> Cowboy Main Wallpaper </a>
+<a href="https://wall.alphacoders.com/by_sub_category.php?id=207430&name=Cowboy+Wallpapers&lang=German"> Cowboy Silhouette </a>
+<a href="https://steamcommunity.com/market/listings/753/713380-The%20Desert"> Desert Background </a>
+<a href="https://myloview.com/wall-mural-8bit-cowboy-on-horse-pixel-art-vector-illustration-wild-west-no-BDBC4A9"> Horse Rider Sprite </a>
+<a href="https://dribbble.com/shots/6218037-Cowboy-Attack"> Cowboy Sprite Inspiration </a>
+<a href="https://www.reddit.com/r/PixelArt/comments/dx8oyi/bullet/"> Bullet Sprite</a>
+
+<h2 class="subheading"> Audio & Font Credit </h2>
+<a href="https://www.youtube.com/watch?v=AFa1-kciCb4&ab_channel=jordanharding"> Music/Main Theme</a>
+<a href="https://www.dafont.com/brotherland.font?text=Cul+Cawboj&psize=l"> Brotherland Font </a>
+<a href="https://fonts.adobe.com/fonts/gin"> Gin Font </a>
+
+<h2 class="subheading"> Fair Use Notice </h2>
+<p id="fairuse">
+  Copyright Disclaimer under section 107 of the Copyright Act 1976, allowance is made for “fair use” for purposes such as criticism, comment, news reporting, teaching, scholarship, education and research.
+  Fair use is a use permitted by copyright statute that might otherwise be infringing.
+</p>
+
+</div>
+</body>
+</html>
