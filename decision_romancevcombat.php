88dbdb1f44db57ecd76aef91c2769b57dc1cea7d
Bodie <86332414+Bod1eF@users.noreply.github.com>
2022-07-25 13:58:56 -0700

    Update decision_romancevcombat.php

--- decision_romancevcombat.php
+++ decision_romancevcombat.php
@@ -2,21 +2,21 @@
 session_start();
 require_once "sql_config.php";
 
-//check that user is signed in
-// if (isset($_SESSION["email"]) && isset($_SESSION["player_id"])) {
-//   try {
-//     $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
-//     $sth = $dbh->prepare("UPDATE player_character SET `position`= 3 
-//       WHERE id =:player_id");
-//     $sth->bindValue(':player_id', $_SESSION["player_id"]);
-//     $sth->execute();
-//     }
-//   catch (PDOException $e) {
-//     echo "<p>Error: {$e->getMessage()}</p>";          
-//   }
-// }else {
-//     header('Location: login.php'); //if user isn't signed in send to login
-// }
+check that user is signed in
+if (isset($_SESSION["email"]) && isset($_SESSION["player_id"])) {
+  try {
+    $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
+    $sth = $dbh->prepare("UPDATE player_character SET `position`= 3 
+      WHERE id =:player_id");
+    $sth->bindValue(':player_id', $_SESSION["player_id"]);
+    $sth->execute();
+    }
+  catch (PDOException $e) {
+    echo "<p>Error: {$e->getMessage()}</p>";          
+  }
+}else {
+    header('Location: login.php'); //if user isn't signed in send to login
+}
 ?>
 
 <!DOCTYPE html>
