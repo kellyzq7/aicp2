7dc8bf41910a4a4b842de9d9a6e7c0365bad6280
Bodie <86332414+Bod1eF@users.noreply.github.com>
2022-07-25 15:08:34 -0700

    Create end.php

---
+++ end.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+require_once "sql_config.php";
+$_SESSION["characer_status"] = "inactive";//set a variable that can be checked in class_select.php to see if a new character needs to be created
+//check that user is signed in
+if (isset($_SESSION["email"]) && isset($_SESSION["player_id"])) {
+  try {
+    $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
+    $sth = $dbh->prepare("UPDATE player_character SET `position`= 23, `isActive` = false
+      WHERE id =:player_id");
+    $sth->bindValue(':player_id', $_SESSION["player_id"]);
+    $sth->execute();
+    }
+  catch (PDOException $e) {
+    echo "<p>Error: {$e->getMessage()}</p>";
+  }
+}else {
+    header('Location: login.php'); //if user isn't signed in send to login
+}
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <title>Cul Cavboj</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+  </head>
+  <body>
+
+
+  <h4>Congratulations on completing the Game. If you would like to explor a new path...<a href = 'class_select.php'><input type = 'button' value = 'create a new character' /></a></h4>
+
+
+
+  </body>
+<html>
