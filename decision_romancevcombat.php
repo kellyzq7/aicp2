b71d4c287ed0650f2b2916a7fcd50ef2d343aaa0
kellyzq7 <kellyzq7@gmail.com>
2022-07-25 09:45:14 -0700

    adding css

--- decision_romancevcombat.php
+++ decision_romancevcombat.php
@@ -3,20 +3,20 @@ session_start();
 require_once "sql_config.php";
 
 //check that user is signed in
-if (isset($_SESSION["email"]) && isset($_SESSION["player_id"])) {
-  try {
-    $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
-    $sth = $dbh->prepare("UPDATE player_character SET `position`= 3 
-      WHERE id =:player_id");
-    $sth->bindValue(':player_id', $_SESSION["player_id"]);
-    $sth->execute();
-    }
-  catch (PDOException $e) {
-    echo "<p>Error: {$e->getMessage()}</p>";          
-  }
-}else {
-    header('Location: login.php'); //if user isn't signed in send to login
-}
+// if (isset($_SESSION["email"]) && isset($_SESSION["player_id"])) {
+//   try {
+//     $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
+//     $sth = $dbh->prepare("UPDATE player_character SET `position`= 3 
+//       WHERE id =:player_id");
+//     $sth->bindValue(':player_id', $_SESSION["player_id"]);
+//     $sth->execute();
+//     }
+//   catch (PDOException $e) {
+//     echo "<p>Error: {$e->getMessage()}</p>";          
+//   }
+// }else {
+//     header('Location: login.php'); //if user isn't signed in send to login
+// }
 ?>
 
 <!DOCTYPE html>
@@ -30,13 +30,15 @@ if (isset($_SESSION["email"]) && isset($_SESSION["player_id"])) {
     <h1>Decision: Romance or Combat</h1>
       <div id = "container">
         <div id = "romance">
-            <img src = "img/romance.png" alt = "romance" />
+            <h2>Romance</h2>
+            <p></p>
             <a href = "romanceEncounter.php"><input type = "button" id = "ready" name = "ready" value = "Choose" /></a>
         </div>
         
         <div id = "combat">
-            <img src = "img/combat.png" alt = "combat" />
-            <a href = "combat1.php"><input type = "button" id = "ready" name = "ready" value = "Choose" /></a>
+          <h2>Combat</h2>
+          <p></p>
+          <a href = "combat1.php"><input type = "button" id = "ready" name = "ready" value = "Choose" /></a>
         </div>
       </div>
     <a href="logout.php"><input type = 'button' value = 'Save and Log Out' /></a>
