32988927c9caef3c3a98d1f5ddae96d7616e1ffe
Kelly Jia <51812358+kellyzq7@users.noreply.github.com>
2022-07-26 12:54:59 -0700

    Update register.php

--- register.php
+++ register.php
@@ -10,7 +10,33 @@ session_start();
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script></script>
 <style>
+@font-face {
+  font-family: brotherland;
+  src: url("fonts/Brotherlands.ttf");
+}
+h1 {
+  font-size:64px;
+}
+body {
+  font-family:sans-serif;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  flex-direction:column;
+}
 
+a {
+  text-align:center;
+  display: block;
+  margin:20px;
+  font-size:36px;
+}
+input {
+  margin-right:20px;
+}
+form, input {
+  font-size:36px;
+}
 </style>
 </head>
 <body>
