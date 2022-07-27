b28a5612a4acb2224d865dc37abadd8588a53426
kellyzq7 <kellyzq7@gmail.com>
2022-07-27 09:31:47 -0700

    register css

--- register.php
+++ register.php
@@ -9,35 +9,7 @@ session_start();
 <meta name="viewport" content="width=device-width">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script></script>
-<style>
-@font-face {
-  font-family: brotherland;
-  src: url("fonts/Brotherlands.ttf");
-}
-h1 {
-  font-size:64px;
-}
-body {
-  font-family:sans-serif;
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  flex-direction:column;
-}
-
-a {
-  text-align:center;
-  display: block;
-  margin:20px;
-  font-size:36px;
-}
-input {
-  margin-right:20px;
-}
-form, input {
-  font-size:36px;
-}
-</style>
+<link rel = "stylesheet" href = "cssandjs/login.css">
 </head>
 <body>
 <h1> Register an Account </h1>
