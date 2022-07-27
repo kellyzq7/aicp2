93fb097b7cb883b05800d6852ccfaa9395dd1969
Bodie <86332414+Bod1eF@users.noreply.github.com>
2022-07-27 11:06:12 -0700

    Update register.php

--- register.php
+++ register.php
@@ -9,17 +9,68 @@ session_start();
 <meta name="viewport" content="width=device-width">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script></script>
-<link rel = "stylesheet" href = "cssandjs/login.css">
+<style>
+
+@font-face {
+  font-family: brotherland;
+  src: url("fonts/Brotherlands.ttf");
+}
+
+@font-face {
+  font-family: Gin;
+  src: url("fonts/gin.otf");
+}
+
+h1 {
+  font-family:brotherland;
+  color:#430804;
+  font-size:72px;
+}
+body {
+  font-family:Gin;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  flex-direction:column;
+  background-color:#F5D3B8;
+}
+
+a {
+  font-family:Gin;
+  color:#D8513D;
+  text-align:center;
+  display: block;
+  margin:30px;
+  font-size:36px;
+}
+input, label {
+  margin-bottom:1.4vmax;
+  font-family:Gin;
+  padding:6px;
+  color:#2A2B2A;
+  font-size:36px;
+}
+form {
+display:flex;
+align-items:center;
+flex-direction:column;
+}
+</style>
 </head>
 <body>
 <h1> Register an Account </h1>
 <form action="login.php" method="POST">
+
+<div>
 <label for="email"> Email: </label>
 <input required type="email" name="email" id="email">
-<br />
+</div>
+
+<div>
 <label for="password"> Password: </label>
 <input required type="password" name="password" id="password">
-<br />
+</div>
+
 <input type="submit" value="Create Account">
 </form>
 </body>
