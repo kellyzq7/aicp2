25245fdd8acdbffc8c578c3bd963479b22732d02
kellyzq7 <kellyzq7@gmail.com>
2022-07-27 09:32:37 -0700

    Update register.php

--- register.php
+++ register.php
@@ -16,10 +16,10 @@ session_start();
 <form action="login.php" method="POST">
 <label for="email"> Email: </label>
 <input required type="email" name="email" id="email">
-
+<br />
 <label for="password"> Password: </label>
 <input required type="password" name="password" id="password">
-
+<br />
 <input type="submit" value="Create Account">
 </form>
 </body>
