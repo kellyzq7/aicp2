9c396cbd3bfc2103c259fd5d9bbd1597e65875a1
Bodie <86332414+Bod1eF@users.noreply.github.com>
2022-07-28 11:06:35 -0700

    Update decision_romancevcombat.php

--- decision_romancevcombat.php
+++ decision_romancevcombat.php
@@ -6,13 +6,13 @@ require_once "sql_config.php";
 if (isset($_SESSION["email"]) && isset($_SESSION["player_id"])) {
   try {
     $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
-    $sth = $dbh->prepare("UPDATE player_character SET `position`= 3 
+    $sth = $dbh->prepare("UPDATE player_character SET `position`= 3
       WHERE id =:player_id");
     $sth->bindValue(':player_id', $_SESSION["player_id"]);
     $sth->execute();
     }
   catch (PDOException $e) {
-    echo "<p>Error: {$e->getMessage()}</p>";          
+    echo "<p>Error: {$e->getMessage()}</p>";
   }
 }else {
     header('Location: login.php'); //if user isn't signed in send to login
@@ -28,20 +28,18 @@ if (isset($_SESSION["email"]) && isset($_SESSION["player_id"])) {
   </head>
   <body>
     <h1>Decision: Romance or Combat</h1>
-    <p>You survived an ambush! Impressive. Now you must choose the rest of your journey.</p>
+    <p>You survived an ambush! Impressive. Now you must choose how to proceed.</p>
       <div id = "container">
 
         <div id = "romance">
             <h2>Romance</h2>
-            <p><strong>Are you seeking true love? Perhaps after a disastrous previous relationship? Go no further and choose romance
-            to earn romance points and pursue something you thought to be impossible to achieve!</strong></p>
+            <p><strong>Approach the remaining bandits with an open heart.</strong></p>
             <br /><br /><a href = "romanceEncounter.php"><input type = "button" id = "ready" name = "ready" value = "Choose" /></a>
         </div>
 
         <div id = "combat">
           <h2>Combat</h2>
-          <p><strong>Want to show off those shiny combative skills? Prove your worth in live combat?
-          Choose combat to harness your skills and earn combat points!</strong></p>
+          <p><strong>Approach the remaining bandits with revolver in hand.</strong></p>
           <a href = "combat1.php"><input type = "button" id = "ready" name = "ready" value = "Choose" /></a>
         </div>
       </div>
