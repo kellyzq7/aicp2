cf63b2847de2a8c35f66c0227a93c3988e519d27
Bodie <86332414+Bod1eF@users.noreply.github.com>
2022-07-27 23:28:56 -0700

    Update freedom.js

--- cssandjs/freedom.js
+++ cssandjs/freedom.js
@@ -1,7 +1,8 @@
 $(document).ready(() => {
 
-let timer = setTimeout(displayFinal, 11000);
+let timer = setTimeout(displayFinal, 11000); 
 
+//after 11 seconds display final message
 function displayFinal() {
   $(".create").removeClass("hidden");
   $("#final").removeClass("hidden");
