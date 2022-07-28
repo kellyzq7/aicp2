d9a76374d492bd588bfde059826c6175a6b62a17
kellyzq7 <kellyzq7@gmail.com>
2022-07-27 21:43:11 -0700

    Update town1.php

--- town1.php
+++ town1.php
@@ -21,13 +21,39 @@ if (isset($_SESSION["email"]) && isset($_SESSION["player_id"])) {
 <!DOCTYPE html>
 <html lang="en-US">
   <head>
-    <title>Decision: Town 1 or Town 2</title>
+    <title>Cul Cavboj</title>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-    <link rel="stylesheet" href="cssandjs/towns.css">
+    <style>
+      @font-face {
+        font-family: Brotherland;
+        src: url(brotherland.ttf);
+      }
+      
+      @font-face {
+        font-family: Gin;
+        src: url(carnival.otf);
+      }
+      
+      body{
+        background-image: url("https://consciouslab.files.wordpress.com/2016/05/forking-paths.jpg");
+        background-size: cover;
+      }
+      
+      h1{
+        font-family: "Brotherland";
+        font-size: 50px;
+        text-align: center;
+        background-color: #d49c68;
+        padding: 2%;
+        border: 6px solid black;
+      }
+    </style>
   </head>
   <body>
-    <h1>Vaquero Valley</h1>
-      
+      <h1>Welcome to Vaquero Valley, click anywhere on image to walk</h1>
+      <a href = 'npc1.php'>
+      <img src = "https://storage.googleapis.com/production-hostgator-v1-0-8/148/412148/0mPbwXcy/18cdb98a2a044f2da36bba895d071701" height = 100% width = 100% alt = "Vaquero Valley" />
+      </a>
     <a href="logout.php"><input type = 'button' value = 'Save and Log Out' /></a>
   </body>
 </html>
